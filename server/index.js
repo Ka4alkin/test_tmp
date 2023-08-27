6d85349a940fdf14e7d9e7e2dbadacd9ce05bf31
Nazar <doskoch25@gmail.com>
2023-08-28 01:25:23 +0300

    update Dockerfile added Insomnia_2023-08-100.json

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
-require('./models/actors-movies-model');
+require('./src/models/actors-movies-model');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const helmet = require('helmet');
-const usersRoutes = require('./routes/users-routes');
-const actorsRoutes = require('./routes/actors-routes');
-const moviesRoutes = require('./routes/movies-routes');
-const sessionsRoutes = require('./routes/sessions-routes');
-const notFound = require('./middlewares/not-found-middleware');
-const errorHandler = require('./middlewares/error-handler-middleware');
+const usersRoutes = require('./src/routes/users-routes');
+const actorsRoutes = require('./src/routes/actors-routes');
+const moviesRoutes = require('./src/routes/movies-routes');
+const sessionsRoutes = require('./src/routes/sessions-routes');
+const notFound = require('./src/middlewares/not-found-middleware');
+const errorHandler = require('./src/middlewares/error-handler-middleware');
 const cors = require('cors');
 
 
@@ -32,9 +32,7 @@ app.use('/movies', moviesRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
-
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+const PORT = process.env.APP_PORT;
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
 });

d1b13bedf0532f7dfd5f7672911e38764a454e7e
Nazar <doskoch25@gmail.com>
2023-08-28 00:06:48 +0300

    added movie delete

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({movie, onDelete}) => {
+  const _handleDelete = () => {
+    onDelete();
+  };
+
   return (
     <div className="bg-white p-4 rounded shadow-md w-full">
-      <h3 className="text-lg font-semibold text-gray-800">{movie.title}</h3>
+      <div className="flex justify-between items-center mb-2">
+        <h3 className="text-lg font-semibold text-gray-800">{movie.title}</h3>
+        <button
+          className="text-red-500 hover:text-red-700"
+          onClick={_handleDelete}
+        >
+          &times;
+        </button>
+      </div>
       <p className="text-gray-600">Year: {movie.year}</p>
       <p className="text-gray-600">Format: {movie.format}</p>
-      {!!movie?.Actors?.length && <>
-        <h4 className="text-md font-medium text-gray-600 mt-2">Actors:</h4>
-        <ul className="list-disc list-inside mt-1">
-          {movie.Actors.map((actor) => (
-            <li key={actor.id}>{actor.name} {actor.surname}</li>
-          ))}
-        </ul>
-      </>}
+      {!!movie?.Actors?.length && (
+        <>
+          <h4 className="text-md font-medium text-gray-600 mt-2">Actors:</h4>
+          <ul className="list-disc list-inside mt-1">
+            {movie.Actors.map((actor) => (
+              <li key={actor.id}>{actor.name} {actor.surname}</li>
+            ))}
+          </ul>
+        </>
+      )}
+
     </div>
   );
 };
--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import axiosInstance from '../axiosInstance.js';
+import {alertErr, alertSuccess} from '../utils.js';
+import {fetchMovies} from '../store/actions/movieActions.js';
+import {useDispatch} from 'react-redux';
 
 const MovieList = ({movies}) => {
+  const dispatch = useDispatch();
+
+  const _onMovieDelete = async (id) => {
+    await axiosInstance.delete(`/movies/${id}` )
+        .then(() => {
+          alertSuccess(`Deleted successfully`);
+        })
+        .then(()=>{
+          dispatch(fetchMovies('0'));
+        })
+        .catch((error) => {
+          alertErr(error);
+        });
+  };
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-1 lg:grid-cols-1">
       {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
+        <MovieCard key={movie.id} movie={movie} onDelete={()=>_onMovieDelete(movie.id)} />
       ))}
     </div>
   );
